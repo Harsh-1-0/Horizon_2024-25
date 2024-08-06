c175d782d6064036dce6e0087731bb3d89d74f1e
AbhinavHarsh3215 <127244024+AbhinavHarsh3215@users.noreply.github.com>
2024-08-07 00:22:10 +0530

    technica and cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import Slogan from "./components/Slogan2.jsx"
-import Technica from "./components/technica" 
+import Slogan from "./Components/Slogan2";
+
 import Navbar from "./component/Navbar"
 import Card from "./Components/Card";
 import Carousel from "./Components/Carousel";
+import Technica from "./Components/technica";
 const cardData = [
   { title: 'Card 1', description: 'This is card 1', image: 'url1' },
   { title: 'Card 2', description: 'This is card 1', image: 'url1' },
@@ -14,26 +15,25 @@ const cardData = [
 function App() {
   return (
     <>
- <div >
-    <Navbar />
-    <div>
-    <Technica><div>One of the Biggest Technical Fests in India <span className="bolder">is back in 2024</span></div>
-    {<div className="mt-2 sm:mt-5">Sed vestibulum diam auctor, tincidunt <span className="bolder">mi nec</span></div>}
-    </Technica>
-    <Slogan>INNOVATE | IDEATE | INSPIRE</Slogan>
-    </div>
-    </div>
-    <div className="entire bg-[#FEFBEC]">
-    <div className="workshops bg-[#B5DDC2] h-[177px] border-t-2 border-b-2 border-black flex justify-center items-center">
-      <p className="text-[120px] font-bold leading-[144px] text-left font-itc-tiffany">WORKSHOPS</p>
-    </div>
+  <div >
+      <Navbar />
+        <div>
+          <Technica><div>One of the Biggest Technical Fests in India <span className="bolder">is back in 2024</span></div>
+          {<div className="mt-2 sm:mt-5">Sed vestibulum diam auctor, tincidunt <span className="bolder">mi nec</span></div>}
+          </Technica>
+          <Slogan>INNOVATE | IDEATE | INSPIRE</Slogan>
+          <div className="entire bg-[#FEFBEC]">
+          <div className="workshops bg-[#B5DDC2] h-[177px] border-t-2 border-b-2 border-black flex justify-center items-center">
+            <p className="text-[120px] font-bold leading-[144px] text-left font-itc-tiffany">WORKSHOPS</p>
+          </div>
+          </div >
+          {/* <Carousel cardData={cardData} /> */}
+        </div>
+</div>
+      
+        {/* her we will use map function to get multiple cards and make a */}
+        
 
-    <div >
-      {/* her we will use map function to get multiple cards and make a */}
-      <Carousel cardData={cardData} />
-
-      </div>
-      </div>
     
     </>
   );
