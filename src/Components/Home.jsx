e2a9cd3796cc38b40c388556f42600abfc96e134
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2024-08-15 22:49:52 +0530

    update home page for laptop

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,34 +3,33 @@ import "../index.css";
 function Home() {
   return (
     <div>
-      <div className="hidden lg:block h-screen w-full text-[#1E1E1E]  bg-[#b5ddc2]">
+      <div className="hidden lg:block w-full text-[#1E1E1E]  bg-[#b5ddc2]">
         <div className=" w-full bg-[#fefbec] flex justify-center items-center">
           
           <p className=" font-main text-[13.6vw]"> HORIZON&apos;24</p>
         </div>
-        <div className="flex  w-full bg-[#b5ddc2] items-center h-2/3 border-t-black border-b-black border-2">
-          <div className="w-2/3 h-full border-black border-r-2  p-12 block text-5xl font-poppins">
-            <br /> <br />
-            One of the Biggest Technical
-            <span className="block  text-5xl font-poppins "></span>
-            <br />
-            <span className="block text-5xl  font-poppins ">
-              Fests in India{" "}
-              <span className="font-extrabold ">is back in 2024</span>
+        <div className="flex  w-full bg-[#b5ddc2] items-center h-2/3 border-t-black border-b-black border-2 ">
+          <div className="w-2/3 h-full   p-12 block text-5xl font-poppins">
+            
+            
+            <span className=" text-5xl font-poppins ">One of the Biggest Technical</span>
+            <span className="block text-5xl  font-poppins pt-8">
+              Fests in India
+              <span className="font-extrabold "> is back in 2024</span>
             </span>
             <br />
-            <span className="block text-3xl mb-[6vh] font-poppins ">
+            <span className="block text-3xl pb-[6vh] font-poppins ">
               See you at Horizon!
             </span>
             <br />
             <button
               type="submit"
-              className="bg-[#F2C6AB] px-6 py-4 border-black border-2 rounded-full text-2xl"
+              className="bg-[#F2C6AB] px-6 py-4 border-black border-2 rounded-full text-2xl font-normal"
             >
               Register Now
             </button>
           </div>
-          <div className="w-1/3 h-full flex items-center justify-center relative overflow-hidden p-8">
+          <div className="w-1/3 h-full flex items-center justify-center relative overflow-hidden p-8 border-black border-l-2">
             <img className="" src="/homepage.png" />
           </div>
         </div>
