8b358cf2618c644c83b24d08069db1b7d2e58487
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2024-08-15 23:19:36 +0530

    raghav

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,34 +3,31 @@ import "../index.css";
 function Home() {
   return (
     <div>
-      <div className="hidden lg:block w-full text-[#1E1E1E]  bg-[#b5ddc2]">
+      <div className="hidden lg:block w-full h-full text-[#1E1E1E]  bg-[#b5ddc2]">
         <div className=" w-full bg-[#fefbec] flex justify-center items-center">
-          
           <p className=" font-main text-[13.6vw]"> HORIZON&apos;24</p>
         </div>
-        <div className="flex  w-full bg-[#b5ddc2] items-center h-2/3 border-t-black border-b-black border-2 ">
-          <div className="w-2/3 h-full   p-12 block text-5xl font-poppins">
-            
-            
-            <span className=" text-5xl font-poppins ">One of the Biggest Technical</span>
+        <div className="flex  w-full bg-[#b5ddc2] items-center h-1/4 border-t-black border-b-black border-2 ">
+          <div className="w-3/5 px-12 block text-5xl font-poppins -mt-16">
+            <span className="-pt-8 text-5xl font-poppins ">One of the Biggest Technical</span>
             <span className="block text-5xl  font-poppins pt-8">
               Fests in India
               <span className="font-extrabold "> is back in 2024</span>
             </span>
             <br />
-            <span className="block text-3xl pb-[6vh] font-poppins ">
+            <span className="block text-3xl pb-[8vh] font-poppins ">
               See you at Horizon!
             </span>
-            <br />
+            
             <button
               type="submit"
-              className="bg-[#F2C6AB] px-6 py-4 border-black border-2 rounded-full text-2xl font-normal"
+              className="bg-[#F2C6AB] px-6 border-black border-2 rounded-full text-2xl font-normal"
             >
               Register Now
             </button>
           </div>
-          <div className="w-1/3 h-full flex items-center justify-center relative overflow-hidden p-8 border-black border-l-2">
-            <img className="" src="/homepage.png" />
+          <div className="w-2/5  flex items-center justify-center relative overflow-hidden p-8 border-black border-l-2">
+            <img className="size-[65vh]" src="/homepage.png" />
           </div>
         </div>
       </div>
@@ -41,9 +38,9 @@ function Home() {
             HORIZON&apos;24
           </p>
         </div>
-        <div className="w-full items-center h-[84vh] lg:border-t-black lg:border-b-black lg:border-2">
+        <div className="w-full items-center h-[84vh] border-t-black border-b-black border-2">
           <div className="w-full h-full border-black flex flex-col justify-evenly">
-            <div className="h-[25vh] mt-[5vh] md:mt-[7vh] md:text-center px-20">
+            <div className="h-[25vh] md:mt-[7vh] md:text-center px-20">
               <span
                 className="text-xl md:text-4xl font-poppins text-[#1E1E1E];
 "
@@ -68,7 +65,7 @@ function Home() {
               <img src="/homepage.png" alt="img" className="size-64" />
               <button
                 type="submit"
-                className="bg-[#F2C6AB]  text-[#1E1E1E] mt-4 border-black border-1 rounded-xl text-xl md:mt-16 w-2/5 -py-4"
+                className="bg-[#F2C6AB]  text-[#1E1E1E] mt-12 border-black border-1 rounded-xl font-normal md:mt-16 w-2/5 -py-4"
               >
                 Register Now
               </button>
